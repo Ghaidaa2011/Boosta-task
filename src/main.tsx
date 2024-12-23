8d076df92b6ed03b536a2984c3ef9723663fdb1f
Ghaidaa <117023274+Ghaidaa2011@users.noreply.github.com>
2024-12-23 23:19:33 +0200

    first commit

--- src/main.tsx
+++ src/main.tsx
@@ -13,6 +13,8 @@ import MainLayout from "./layouts/MainLayout";
 import { I18nextProvider } from "react-i18next";
 import i18next from "./i18n.ts";
 import { TranslateProvider } from "./context/TranslateContext.tsx";
+//flowbite
+import "flowbite";
 
 createRoot(document.getElementById("root")!).render(
   <I18nextProvider i18n={i18next}>
