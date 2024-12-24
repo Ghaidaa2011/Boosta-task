e06f48ce73c9dd03c305bd8a7e7f9a37b6e96737
Ghaidaa <117023274+Ghaidaa2011@users.noreply.github.com>
2024-12-24 19:28:34 +0200

    doneee

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,29 @@
 import flowbite from "flowbite-react/tailwind";
 
+/** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", flowbite.content()],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/flowbite/**/*.js",
+    flowbite.content()
+  ],
   theme: {
     extend: {
       colors: {
-        customRed: "#E30613", // Convert RGB to Hex
+        customRed: "#E30613",
       },
     },
     screens: {
       'xs': '480px',
-      
       'sm': '640px',
-
       'md': '768px',
-
       'lg': '1024px',
-
       'xl': '1280px',
-
       '2xl': '1536px',
     }
   },
   plugins: [
-    // ...
-    flowbite.plugin(),
+    flowbite.plugin()
   ],
 };
